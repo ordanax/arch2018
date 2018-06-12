b22aa1297d6a40118b3cc9c2b0f1ef5fe6888ee2
ordanax <ordanax@ya.ru>
2018-06-12 21:19:29 +0500

    Create arch_3.sh

---
+++ arch_3.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo 'Установка AUR (aurman)'
+sudo pacman -Syy
+sudo pacman -S git --noconfirm
+git clone https://aur.archlinux.org/aurman.git
+cd aurman
+makepkg -si --noconfirm
+cd ..
+rm -rf aurman
+
+echo 'Установка программ'
+sudo pacman -S firefox ufw obs-studio veracrypt freemind filezilla cherrytree gimp libreoffice libreoffice-fresh-ru kdenlive audacity pidgin screenfetch vlc qt4 qbittorrent f2fs-tools dosfstools ntfs-3g alsa-lib alsa-utils gnome-calculator file-roller p7zip unrar gvfs aspell-ru pulseaudio --noconfirm 
+aurman -S dropbox joxi obs-linuxbrowser xflux xflux-gui-git purple-vk-plugin purple-facebook pidgin-encryption sublime-text2 hunspell-ru pamac-aur --noconfirm 
+
+echo 'Установка тем'
+aurman -S osx-arc-shadow breeze-obsidian-cursor-theme papirus-maia-icon-theme-git --noconfirm
+
+echo 'Качаем и устанавливаем настройки Xfce'
+# Чтобы сделать копию ваших настоек XFCE выполните команду ниже.
+# tar -czf xfce4.tar.gz ~/.config/xfce4
+# Выгрузите архив в интернет и скорректируйте ссылку на XFCE файл заменив ссылку http://ordanax.ru/arch_linux/xfce4.tar.gz на свою.
+cd ~/Downloads
+wget http://ordanax.ru/arch/xfce4.tar.gz
+tar -xzf xfce4.tar.gz -C /
+rm -rf ~/Downloads/*
+
+echo 'Включаем сетевой экран'
+sudo ufw enable
+
+echo 'Установка завершена!'
+rm -rf arch_3.sh
