17fc7ed5d6515e996386f38654fbf8f88d4fbb6e
gitumium <gitumium@mail.ru>
2018-06-18 06:41:00 +0300

    Update arch_3.sh

--- arch_3.sh
+++ arch_3.sh
@@ -18,12 +18,12 @@ cd ..
 rm -rf aurman
 
 echo 'Установка программ'
-sudo pacman -S firefox ufw --noconfirm
-# obs-studio veracrypt freemind filezilla cherrytree gimp libreoffice libreoffice-fresh-ru kdenlive audacity pidgin screenfetch vlc qt4 qbittorrent f2fs-tools dosfstools ntfs-3g alsa-lib alsa-utils gnome-calculator file-roller p7zip unrar gvfs aspell-ru pulseaudio --noconfirm 
-# aurman -S dropbox joxi obs-linuxbrowser xflux xflux-gui-git purple-vk-plugin purple-facebook pidgin-encryption sublime-text2 hunspell-ru pamac-aur --noconfirm 
+sudo pacman -S dmenu scrot feh firefox ufw pidgin ntfs-3g screenfetch alsa-lib alsa-utils p7zip unrar pulseaudio --noconfirm
+# obs-studio veracrypt freemind filezilla cherrytree gimp libreoffice libreoffice-fresh-ru kdenlive audacity screenfetch vlc qt4 qbittorrent f2fs-tools dosfstools ntfs-3g gnome-calculator file-roller gvfs aspell-ru pulseaudio --noconfirm 
+aurman -S dropbox joxi obs-linuxbrowser purple-vk-plugin purple-facebook pidgin-encryption sublime-text2 hunspell-ru pamac-aur --noconfirm 
 
 echo 'Установка тем'
-aurman -S osx-arc-shadow breeze-obsidian-cursor-theme papirus-maia-icon-theme-git --noconfirm
+aurman -S compton lxappearance terminus-ttf xkblayout-state ttf-font-awesome --noconfirm
 
 echo 'Создаем нужные директории.'
 mkdir ~/Downloads
@@ -33,24 +33,6 @@ mkdir ~/Video/
 mkdir ~/Dropbox/
 mkdir ~/Dropbox/WALLPAPERS
 mkdir ~/Dropbox/WALLPAPERS/GREEN/
-
-echo 'Качаем и устанавливаем настройки Xfce'
-# Чтобы сделать копию ваших настоек XFCE выполните команду ниже.
-# tar -czf xfce4.tar.gz ~/.config/xfce4
-# Выгрузите архив в интернет и скорректируйте ссылку на XFCE файл заменив ссылку http://ordanax.ru/arch_linux/xfce4.tar.gz на свою.
-cd ~/Downloads
-wget http://ordanax.ru/arch/xfce4.tar.gz
-#rm -rf ~/.config/xfce4
-tar -xzf xfce4.tar.gz -C /
-
-echo 'Ставим лого ArchLinux в меню'
-wget ordanax.ru/arch/archlinux_logo.png
-sudo mv -f ~/Downloads/archlinux_logo.png /usr/share/pixmaps/archlinux_logo.png
-
-echo 'Ставим обои на рабочий стол'
-wget ordanax.ru/arch/Deepin_Linux_Manjaro.jpg
-sudo mv -f ~/Downloads/Deepin_Linux_Manjaro.jpg ~/Dropbox/WALLPAPERS/GREEN/Deepin_Linux_Manjaro.jpg
-rm -rf ~/Downloads/*
 
 echo 'Включаем сетевой экран'
 sudo ufw enable
