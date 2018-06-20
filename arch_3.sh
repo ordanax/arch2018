ebe2e6d3544dfe45c395226148f423701ab4278e
gitumium <gitumium@mail.ru>
2018-06-20 06:35:09 +0500

    Update arch_3.sh

--- arch_3.sh
+++ arch_3.sh
@@ -21,13 +21,13 @@ cd ..
 rm -rf aurman
 
 echo 'Установка программ'
-sudo pacman -S nodejs vim openvpn rtorrent mplayer evince --noconfirm
+sudo pacman -S arc-gtk-theme lxappearance nodejs vim openvpn rtorrent mplayer evince --noconfirm
 sudo pacman -S dmenu scrot feh firefox ufw pidgin ntfs-3g screenfetch alsa-lib alsa-utils p7zip unrar pulseaudio --noconfirm
 # okular obs-studio pamac-aur veracrypt freemind filezilla cherrytree gimp libreoffice libreoffice-fresh-ru kdenlive audacity screenfetch vlc qt4 qbittorrent f2fs-tools dosfstools ntfs-3g gnome-calculator file-roller gvfs aspell-ru --noconfirm 
 aurman -S dropbox joxi obs-linuxbrowser purple-vk-plugin purple-facebook pidgin-encryption sublime-text2 hunspell-ru --noconfirm 
 
 echo 'Установка тем'
-aurman -S compton lxappearance terminus-ttf xkblayout-state ttf-font-awesome --noconfirm
+aurman -S compton terminus-ttf xkblayout-state ttf-font-awesome --noconfirm
 
 echo 'Создаем нужные директории.'
 mkdir ~/Downloads
