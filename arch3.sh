a244fe4f2a2c9aebb269df34fda0f5760cf070a7
ordanax <ordanax@ya.ru>
2019-03-18 01:57:37 +0500

    yay

--- arch3.sh
+++ arch3.sh
@@ -11,33 +11,7 @@ sudo pacman -S firefox ufw --noconfirm
 # yay -S dropbox flameshot-git obs-linuxbrowser xflux xflux-gui-git sublime-text2 hunspell-ru pamac-aur --noconfirm 
 
 echo 'Установка тем'
-aurman -S osx-arc-shadow breeze-obsidian-cursor-theme papirus-maia-icon-theme-git --noconfirm
-
-echo 'Создаем нужные директории'
-sudo pacman -S xdg-user-dirs
-xdg-user-dirs-update
-
-echo 'Качаем и устанавливаем настройки Xfce'
-# Чтобы сделать копию ваших настоек XFCE выполните команду ниже.
-# tar -czf xfce4.tar.gz ~/.config/xfce4
-# Выгрузите архив в интернет и скорректируйте ссылку на XFCE файл заменив ссылку http://ordanax.ru/arch_linux/xfce4.tar.gz на свою.
-
-# cd ~/Downloads
-# wget http://ordanax.ru/arch/xfce4.tar.gz
-#rm -rf ~/.config/xfce4
-# tar -xzf xfce4.tar.gz -C /
-
-echo 'Ставим лого ArchLinux в меню'
-wget ordanax.ru/arch/archlinux_logo.png
-sudo mv -f ~/Загрузки/archlinux_logo.png /usr/share/pixmaps/archlinux_logo.png
-
-echo 'Ставим обои на рабочий стол'
-wget ordanax.ru/arch/Deepin_Linux_Manjaro.jpg
-sudo mv -f ~/Загрузки/Deepin_Linux_Manjaro.jpg ~/Dropbox/WALLPAPERS/GREEN/Deepin_Linux_Manjaro.jpg
-rm -rf ~/Загрузки/*
-
-echo 'Включаем сетевой экран'
-sudo ufw enable
+yay -S osx-arc-shadow breeze-obsidian-cursor-theme papirus-maia-icon-theme-git --noconfirm
 
 echo 'Установка завершена!'
 rm -rf ~/arch3.sh
