c3a2c44c31e359b70d2019567c41716d67d8b080
ordanax <ordanax@ya.ru>
2019-03-18 02:12:58 +0500

    Update arch3.sh

--- arch3.sh
+++ arch3.sh
@@ -13,5 +13,32 @@ sudo pacman -S firefox ufw --noconfirm
 echo 'Установка тем'
 yay -S osx-arc-shadow breeze-obsidian-cursor-theme papirus-maia-icon-theme-git --noconfirm
 
+echo 'Создаем нужные директории'
+sudo pacman -S xdg-user-dirs
+xdg-user-dirs-update
+
+echo 'Качаем и устанавливаем настройки Xfce'
+# Чтобы сделать копию ваших настоек XFCE выполните команду ниже.
+# tar -czf xfce4.tar.gz ~/.config/xfce4
+# Выгрузите архив в интернет и скорректируйте ссылку на XFCE файл заменив ссылку http://ordanax.ru/arch_linux/xfce4.tar.gz на свою.
+
+# cd ~/Downloads
+# wget http://ordanax.ru/arch/xfce4.tar.gz
+#rm -rf ~/.config/xfce4
+# tar -xzf xfce4.tar.gz -C /
+
+echo 'Ставим лого ArchLinux в меню'
+wget git.io/arch_logo.png
+sudo mv -f ~/Загрузки/arch_logo.png /usr/share/pixmaps/arch_logo.png
+
+echo 'Ставим обои на рабочий стол'
+wget git.io/bg.jpg
+sudo rm -rf /usr/share/backgrounds/xfce/* #Удаляем стандартрые обои
+sudo mv -f ~/Загрузки/bg.jpg /usr/share/backgrounds/xfce/bg.jpg
+rm -rf ~/Загрузки/*
+
+echo 'Включаем сетевой экран'
+sudo ufw enable
+
 echo 'Установка завершена!'
 rm -rf ~/arch3.sh
