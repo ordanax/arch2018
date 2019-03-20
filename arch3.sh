c31ec43762ff064f7b9c6ad4253b261aa18785a0
ordanax <ordanax@ya.ru>
2019-03-21 02:58:06 +0500

    Update arch3.sh

--- arch3.sh
+++ arch3.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 mkdir ~/Downloads
 cd ~/Downloads
+echo 'Установка AUR (yay)'
+sudo pacman -Syy
+sudo pacman -S wget --noconfirm
+wget git.io/yay-install.sh && sh yay-install.sh --noconfirm
 
+echo 'Создаем нужные директории'
+sudo pacman -S xdg-user-dirs --noconfirm
+xdg-user-dirs-update
 
-echo 'Качаем и устанавливаем настройки Xfce'
-# Чтобы сделать копию ваших настоек XFCE перейдите в домашнюю директорию ~/username открйте в этой категории терминал и выполните команду ниже.
-# tar -czf xfce4.tar.gz .config/xfce4
-# Выгрузите архив в интернет и скорректируйте ссылку на XFCE файл заменив ссылку на свою.
+echo 'Установка программ'
+sudo pacman -S firefox ufw qt4 f2fs-tools dosfstools ntfs-3g alsa-lib alsa-utils file-roller p7zip unrar gvfs aspell-ru pulseaudio --noconfirm
+# obs-studio veracrypt freemind filezilla cherrytree gimp libreoffice libreoffice-fresh-ru kdenlive audacity screenfetch vlc qbittorrent gnome-calculator --noconfirm 
+# yay -S dropbox flameshot-git obs-linuxbrowser xflux xflux-gui-git sublime-text2 hunspell-ru pamac-aur --noconfirm 
 
-rm -rf ~/.config/xfce4/
-wget git.io/xfce4.tar.gz
-tar -xzf xfce4.tar.gz -C ~/
+echo 'Установка тем'
+yay -S osx-arc-shadow breeze-obsidian-cursor-theme papirus-maia-icon-theme-git --noconfirm
 
 echo 'Ставим лого ArchLinux в меню'
 wget git.io/arch_logo.png
@@ -20,6 +26,12 @@ echo 'Ставим обои на рабочий стол'
 wget git.io/bg.jpg
 sudo rm -rf /usr/share/backgrounds/xfce/* #Удаляем стандартрые обои
 sudo mv -f ~/Downloads/bg.jpg /usr/share/backgrounds/xfce/bg.jpg
+
+echo 'Включаем сетевой экран'
+sudo ufw enable
+
+echo 'Добавляем в автозагрузку:'
+sudo systemctl enable ufw
 
 sudo rm -rf ~/Downloads
 
