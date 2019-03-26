8f0af5216be5c34edbbca2eff17c5a3e9c860d91
ordanax <ordanax@ya.ru>
2019-03-27 03:16:16 +0500

    Update arch2.sh

--- arch2.sh
+++ arch2.sh
@@ -71,11 +71,11 @@ elif [[ $vm_setting == 2 ]]; then
 fi
 
 echo 'Какой ставим DM'
-read -p "1 - sddm, 2 - lxdm: " vm_setting
-if [[ $vm_setting == 1 ]]; then
+read -p "1 - sddm, 2 - lxdm: " dm_setting
+if [[ $dm_setting == 1 ]]; then
   pacman -Sy sddm sddm-kcm --noconfirm
   systemctl enable sddm.service -f
-elif [[ $vm_setting == 2 ]]; then
+elif [[ $dm_setting == 2 ]]; then
   pacman -S lxdm --noconfirm
   systemctl enable lxdm
 fi
