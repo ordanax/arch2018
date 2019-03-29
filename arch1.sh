b532c7eb0049d900871e472f60b3cd761261cf33
senyagbSl <49067759+senyagbSl@users.noreply.github.com>
2019-03-29 16:43:03 +0200

    Update arch1.sh

--- arch1.sh
+++ arch1.sh
@@ -67,7 +67,7 @@ swapon /dev/sda3
 mount /dev/sda4 /mnt/home
 
 echo '3.1 Выбор зеркал для загрузки. Ставим зеркало от Яндекс'
-echo "Server = http://mirror.yandex.ru/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
+echo "Server = http://archlinux.ip-connect.vn.ua/$repo/os/$arch" > /etc/pacman.d/mirrorlist
 
 echo '3.2 Установка основных пакетов'
 pacstrap /mnt base base-devel
