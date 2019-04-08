fef3bab40d3c79f6aba39bdfa36dc408031e0262
ordanax <ordanax@ya.ru>
2019-04-09 02:03:49 +0500

    Update arch2.sh

--- arch2.sh
+++ arch2.sh
@@ -69,7 +69,7 @@ if [[ $vm_setting == 1 ]]; then
 elif [[ $vm_setting == 2 ]]; then
   pacman -Sy plasma-meta kdebase --noconfirm
 elif [[ $vm_setting == 3 ]]; then  
-  pacman -S  openbox obmenu lxappearance obconf xfce4-terminal nitrogen tint2 thunar mousepad wmctrl compton papirus-icon-theme
+  pacman -S  openbox xfce4-terminal
 fi
 
 echo 'Какой ставим DM ?'
