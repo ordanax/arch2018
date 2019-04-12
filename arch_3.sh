e57c7c060ae10b17e09615e38d3da30f1a6ed66d
gitumium <gitumium@mail.ru>
2019-04-12 13:06:15 +0300

    Update arch_3.sh

--- arch_3.sh
+++ arch_3.sh
@@ -11,25 +11,16 @@ cd ..
 rm -rf expac-git
 
 #Ставим aurman
-git clone https://aur.archlinux.org/aurman.git
-cd aurman
-sudo pacman -S archlinux-keyring
-sudo pacman-key -r 465022E743D71E39
-gpg --recv-keys 465022E743D71E39
-makepkg -si --noconfirm
-cd ..
-rm -rf aurman
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
 
 echo 'Установка программ'
-sudo pacman -S most lxappearance vim rtorrent mplayer ufw nodejs ntfs-3g dmenu feh alsa-lib alsa-utils chromium p7zip unrar pulseaudio --noconfirm
-aurman -S sublime-text-dev arc-gtk-theme openvpn cmus evince openssh sxiv yarn virtualbox imlib2-webp-git --noconfirm
+sudo pacman -S most neofetch lxappearance vim rtorrent mplayer nodejs ntfs-3g dmenu feh alsa-lib alsa-utils chromium p7zip unrar pulseaudio --noconfirm
+yay -S sublime-text-dev arc-gtk-theme polybar ls_extended ttf-roboto-mono openvpn cmus evince openssh sxiv yarn virtualbox imlib2-webp-git --noconfirm
 #sudo pacman -S scrot pidgin screenfetch --noconfirm
 #okular obs-studio veracrypt freemind filezilla cherrytree gimp libreoffice libreoffice-fresh-ru kdenlive audacity screenfetch qt4 f2fs-tools dosfstools gnome-calculator file-roller gvfs aspell-ru --noconfirm 
 #aurman -S dropbox obs-linuxbrowser purple-vk-plugin purple-facebook pidgin-encryption hunspell-ru --noconfirm 
-
-echo 'Установка тем'
-sudo pacman -S neofetch --noconfirm
-aurman -S compton polybar ls_extended ttf-roboto-mono --noconfirm
 
 echo 'Создаем нужные директории.'
 mkdir ~/Downloads
