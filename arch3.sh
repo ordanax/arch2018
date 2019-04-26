b7dacc12945984662594a25b33ef8f54dd242fe0
ordanax <ordanax@ya.ru>
2019-04-26 19:45:50 +0500

    dropbox

--- arch3.sh
+++ arch3.sh
@@ -20,6 +20,8 @@ read -p "1 - Да, 0 - Нет: " prog_set
 if [[ $prog_set == 1 ]]; then
   sudo pacman -S obs-studio veracrypt vlc freemind filezilla cherrytree gimp libreoffice libreoffice-fresh-ru kdenlive audacity screenfetch vlc qbittorrent gnome-calculator --noconfirm
   yay -S timeshift flameshot-git xflux sublime-text-dev hunspell-ru pamac-aur --noconfirm 
+  gpg --keyserver pgp.mit.edu --recv-keys FC918B335044912E
+  yay -S dropbox --noconfirm
 elif [[ $prog_set == 0 ]]; then
   echo 'Установка программ пропущена.'
 fi
