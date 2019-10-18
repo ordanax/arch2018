11faf9267f88638880f41a4052cb09b990f28a72
ordanax <ordanax@ya.ru>
2019-10-19 02:49:17 +0500

    Update arch1.sh

--- arch1.sh
+++ arch1.sh
@@ -70,7 +70,7 @@ echo '3.1 Выбор зеркал для загрузки. Ставим зерк
 echo "Server = http://mirror.yandex.ru/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
 
 echo '3.2 Установка основных пакетов'
-pacstrap /mnt base base-devel linux linux-firmware nano dhcpcd
+pacstrap /mnt base base-devel linux linux-firmware nano dhcpcd netctl
 
 echo '3.3 Настройка системы'
 genfstab -pU /mnt >> /mnt/etc/fstab
